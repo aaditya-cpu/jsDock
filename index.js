78ef1fa36575e615a9aabddc42c0934c09bdc6cb
Death301 <aadirya.uzumaki@gmail.com>
2023-05-26 09:58:12 +0530

    Latest

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("lets Start)")
+const express = require('express')
+const app = express()
+const port = process.env.PORT||3000;
+
+app.get('/', (req, res) => res.send('<h2>Hello noob!</h2>'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
